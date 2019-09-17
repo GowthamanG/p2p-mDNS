5026c3b22a952694342acd124b254cc82696f087
GowthamanG <gowthaman.gobalasingam@gmail.com>
2019-09-17 22:24:18 +0200

    Bugfix, clean code

--- peerDiscovery.js
+++ peerDiscovery.js
@@ -7,18 +7,6 @@ class Peer {
         this.hostname = hostname.toString();
         this.ip = ip.toString();
     }
-
-    updateHostname(hostname){
-
-        if (this.hostname !== hostname)
-            this.hostname = hostname.toString();
-    }
-
-    updateIp(ip){
-
-        if(this.ip !== ip)
-            this.ip = ip.toString();
-    }
 }
 
 let peers = [];
@@ -70,21 +58,27 @@ module.exports = {
                     peers.push(new Peer(response.answers[0].data.target, response.answers[1].data));
                 else {
 
+                    let peerIsInList = false;
 
                     for (let i = 0; i < peers.length; i++) {
                         let currentPeer = peers[i];
 
-                        if (currentPeer.hostname !== response.answers[0].data.target.toString() && currentPeer.ip !== response.answers[1].data.toString()) {
-                            let new_Peer = new Peer(response.answers[0].data.target, response.answers[1].data);
-                            peers.push(new_Peer);
-                        } else if (currentPeer.hostname === response.answers[0].data.target.toString() && currentPeer.ip !== response.answers[1].data.toString()) {
-                            currentPeer.ip = response.answers[1].data.toString();
+                        if (currentPeer.hostname === response.answers[0].data.target && currentPeer.ip === response.answers[1].data) {
+                            peerIsInList = true;
+                            break;
+                        } else if (currentPeer.hostname === response.answers[0].data.target && currentPeer.ip !== response.answers[1].data) {
+                            currentPeer.ip = response.answers[1].data;
                             peers[i] = currentPeer;
-                        } else if (currentPeer.hostname !== response.answers[0].data.target.toString() && currentPeer.ip === response.answers[1].data.toString()) {
-                            currentPeer.hostname = response.answers[0].data.target.toString();
+                            peerIsInList = true;
+                        } else if (currentPeer.hostname !== response.answers[0].data.target && currentPeer.ip === response.answers[1].data) {
+                            currentPeer.hostname = response.answers[0].data.target;
                             peers[i] = currentPeer;
+                            peerIsInList = true;
                         }
                     }
+
+                    if(peerIsInList === false)
+                        peers.push(new Peer(response.answers[0].data.target, response.answers[1].data));
                 }
             }
 
@@ -99,22 +93,4 @@ module.exports = {
     }
 
 };
-/*
-function getPeers(){
-    return peers;
-}
-function createNameRandomly(name){
-    let randomNumber = (Math.random() * 20) + 1;
-    return name.toString().replace('.local', '_' + randomNumber + '.local');
-}
-function updatePeersData(){
-    thisPeer.updateIp(addr.ipv4());
-    peers.forEach(value => {
-       mdns.query(value().hostname.toString(), 'SRV');
-       mdns.on('query', function(query){
-           if(query.address.toString() !== value().ip.toString())
-               value.updateIp(query.address.toString());
-       })
-    });
-}
-*/
+
